f4b4dbfae9cce32cdbbff6b88d80bafc14f888da
Elias-ReactDeveloper <eliasvasques74@gmail.com>
2022-11-25 12:20:25 -0300

    responsividade add post tela pequena

--- src/components/Posts/CreatePost.jsx
+++ src/components/Posts/CreatePost.jsx
@@ -95,6 +95,11 @@ const StyledBox = styled(Box)`
     display: flex;
     flex-direction: column;
     gap: 20px;
+
+    @media(max-width: 600px) {
+        width: 100%;
+        height: 100%;
+    } 
 `
 
 const StyledModal = styled(Modal)`
